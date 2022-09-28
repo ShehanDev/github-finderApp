001e0ba0b9cec695297d9712e6aa9997ac253e0c
Shehan Mallawaarchchi <shehan96mts@gmail.com>
2022-09-28 15:16:58 +0530

    pass the state to user com

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./component/navbar";
+import axios from "axios";
 import Users from "./users/users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    console.log("COm did mount");
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     // const name = "jhon doe ";
     // const loading = false;
@@ -13,7 +26,7 @@ class App extends Component {
         {/* {loading ? <h4>...loading</h4> : <h1>Hello {showName && name}</h1>} */}
         <Navbar icon="fa fa-github" title="GitHuB Finder" />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
--- src/users/users.js
+++ src/users/users.js
@@ -1,37 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import User from "./user";
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "id",
-        login: "pjhyett",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        id: "id",
-        login: "wycats",
-        avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
-        html_url: "https://github.com/ezmobius",
-      },
-    ],
-  };
-  render() {
-    return (
-      <div className="grid-3">
-        {this.state.users.map((user) => (
-          <User key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+
+const Users = (props) => {
+  return (
+    <div className="grid-3">
+      {props.users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 export default Users;
