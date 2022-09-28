6a606f574abb1c060f90d2fe8e70e0c81aae9ab5
Shehan Mallawaarchchi <shehan96mts@gmail.com>
2022-09-28 15:37:57 +0530

    spinner component added

--- src/users/users.js
+++ src/users/users.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Spinner from "../component/layouts/spinner";
 import User from "./user";
 
-const Users = (props) => {
-  return (
-    <div className="grid-3">
-      {props.users.map((user) => (
-        <User key={user.id} user={user} />
-      ))}
-    </div>
-  );
+const Users = ({ users, loading }) => {
+  if (loading) {
+    <Spinner />;
+  } else {
+    return (
+      <div className="grid-3">
+        {users.map((user) => (
+          <User key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Users;
