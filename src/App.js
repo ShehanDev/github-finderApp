2ab197ad4c68ab086875b04c1b9cbe8c173102e0
Shehan Mallawaarchchi <shehan96mts@gmail.com>
2022-09-28 11:09:36 +0530

    "user compnent and pass data from rest api"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Navbar from "./component/navbar";
+import User from "./users/users";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         {/* {loading ? <h4>...loading</h4> : <h1>Hello {showName && name}</h1>} */}
         <Navbar icon="fa fa-github" title="GitHuB Finder" />
+        <User />
       </div>
     );
   }
---
+++ src/users/users.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+class User extends Component {
+    constructor() {
+        super();
+        //set default state of git hub user 
+        this.state = {
+            id: 'id',
+            login: "mojombo",
+            avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+            html_url: "https://github.com/mojombo"
+
+        }
+    }
+
+    render() {
+        const { avatar_url, login, html_url } = this.state;
+        return (
+            <div className='card text-center'>
+                <img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }} />
+                <h3 >{login}</h3>
+                <div >
+                    <a href={html_url} className='btn btn-dark btn-sm my-1'>more</a>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default User;
